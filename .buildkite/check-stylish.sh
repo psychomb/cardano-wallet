99124847c083360e4ca0cc4fd05bc6756fbf4543
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-02-24 05:14:00 +0000

    Remove files that are not relevant to coin selection and fee balancing.

--- .buildkite/check-stylish.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-stylish-haskell -i `git ls-files -- '*.hs'`
-
-git diff --exit-code
--- nix/regenerate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-exec $(nix-build `dirname $0`/. -A regenerateStackPackages --no-out-link)
